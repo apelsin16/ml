485de807092353e8da85a4ef891bb6cdc355e463
Apelsin16 <apelsin@i.ua>
2018-08-03 15:23:49 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
+
+// var elem1 = document.querySelector('.button-collapse');
+// var instance1 = M.Sidenav.getInstance(elem1);
+
+$(document).ready(function(){
+  $('.sidenav').sidenav({
+    menuWidth: 300,
+    closeOnClick: true
+   
+  });
+});
+
+
