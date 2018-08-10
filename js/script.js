a85e802ecc70eba0af10cdbcc694a5267d46e2ce
Apelsin16 <apelsin@i.ua>
2018-08-10 13:42:04 +0300

    template

--- js/script.js
+++ js/script.js
@@ -1,16 +1,67 @@
-var elem = document.querySelector('.collapsible.expandable');
-var instance = M.Collapsible.init(elem, {
-  accordion: false
-});
 
-
+(function(){
 $(document).ready(function(){
   $('.sidenav').sidenav({
     menuWidth: 250,
     closeOnClick: true   
   });
   $('.carousel').carousel();
+  $('.collapsible.expandable').collapsible();
+
+  $.ajax({
+      url: './data.json'
+  }).done(function (data) {
+      var json = JSON.parse(data);
+      console.log(json);
+  })
+
+    // const templateScript = $('services').html();
+    // const template = Handlebars.compile(templateScript);
+    // console.log(template);
 
 });
 
 
+// function createInfoBlock () {
+
+// var container = document.getElementById('container');
+// var template = document.getElementById('template-accordeon');
+// var tmpl = _.template(template.textContent);
+// var compiled = _.template(tmpl);
+
+// console.log(container);
+// console.log(document.getElementById('template-accordeon'));
+// console.log(template);
+// console.log(tmpl);
+// console.log(compiled);
+
+// fetch("../data.json")
+// .then(function(response){
+//   if (response.ok){
+//     return response.json();
+//   }
+//   throw new Error('Error during fetch');
+// })
+// .then(function(data){
+//   var info = _.map(data.articles, function(item){
+//           return {
+//         title:item.title,
+//         body: item.body
+//         };
+
+//   });
+// render(compiled, data, container);
+//   })
+// }
+
+// function render(template, data, parent) {
+//   var htmlString = ' ';
+//   _.forEach(data, function(item) {
+//     htmlString += template(item);
+//   });
+
+// parent.innerHTML = htmlString;
+// }
+
+// createInfoBlock ();
+})();
