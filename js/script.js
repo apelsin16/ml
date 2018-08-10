0450ea5c9de54c7830f35ef30c8abaced75805b3
Apelsin16 <apelsin@i.ua>
2018-08-10 15:34:11 +0300

    template

--- js/script.js
+++ js/script.js
@@ -15,26 +15,17 @@ $(document).ready(function(){
                 throw new Error('Error during fetch');
             })
             .then(function (data) {
-                console.log(data);
+
                 const templateScript = $('services').html();
+                console.log(templateScript);
                 const template = Handlebars.compile(templateScript);
                 const html = template(data);
                 container.append(html);
             })
-    };
+    }
 
 
 
-    // $.ajax({
-    //     url: 'data.json'
-    // }).done(function (data) {
-    //     var json = JSON.parse(data);
-    //     console.log(json);
-    //     var templateScript = $('services').html();
-    //     var template = Handlebars.compile(templateScript);
-    //     var html    = template(json);
-    //     container.append(html);
-    // })
 
     $('.sidenav').sidenav({
     menuWidth: 250,
@@ -48,46 +39,14 @@ $(document).ready(function(){
 });
 
 
-// function createInfoBlock () {
-
-// var container = document.getElementById('container');
-// var template = document.getElementById('template-accordeon');
-// var tmpl = _.template(template.textContent);
-// var compiled = _.template(tmpl);
-
-// console.log(container);
-// console.log(document.getElementById('template-accordeon'));
-// console.log(template);
-// console.log(tmpl);
-// console.log(compiled);
-
-// fetch("../data.json")
-// .then(function(response){
-//   if (response.ok){
-//     return response.json();
-//   }
-//   throw new Error('Error during fetch');
-// })
-// .then(function(data){
-//   var info = _.map(data.articles, function(item){
-//           return {
-//         title:item.title,
-//         body: item.body
-//         };
-
-//   });
-// render(compiled, data, container);
-//   })
-// }
-
-// function render(template, data, parent) {
-//   var htmlString = ' ';
-//   _.forEach(data, function(item) {
-//     htmlString += template(item);
-//   });
-
-// parent.innerHTML = htmlString;
-// }
-
-// createInfoBlock ();
 })();
+// $.ajax({
+//     url: 'data.json'
+// }).done(function (data) {
+//     var json = JSON.parse(data);
+//     console.log(json);
+//     var templateScript = $('services').html();
+//     var template = Handlebars.compile(templateScript);
+//     var html    = template(json);
+//     container.append(html);
+// })
