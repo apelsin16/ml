469af4829555a69ca47dcdfaa22ddea0546d3416
Apelsin16 <apelsin@i.ua>
2018-08-12 11:38:11 +0300

    ready

--- js/script.js
+++ js/script.js
@@ -1,52 +1,1 @@
-
-(function(){
-
-$(document).ready(function(){
-
-    $('.sidenav').sidenav({
-        menuWidth: 250,
-        closeOnClick: true
-    });
-    $('.carousel').carousel();
-    $('.collapsible.expandable').collapsible();
-    $('.tabs').tabs();
-    console.log('Doc ready');
-    // $('div.tab-content').hide().first().show();
-
-    // function tabs () {
-    //     event.preventDefault();
-    //     $(this).toggleClass('active');
-    //     $(this).siblings('.tab').removeClass('active');
-    //     var divID = $(this).children().attr('href');
-    //     $('div.tab-content').hide();
-    //     $(divID).show();
-    // }
-    // $('.tab').click(tabs);
-    // $('.tab').focus(tabs);
-});
-
-
-
-function createInfoBlock () {
-        const container = $('#container');
-
-        fetch('data.json')
-            .then(function (response) {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Error during fetch');
-            })
-            .then(function (data) {
-
-                const templateScript = document.getElementById("services").innerHTML;
-                const template = Handlebars.compile(templateScript);
-
-                const html = template(data);
-                container.append(html);
-            })
-    };
-createInfoBlock();
-console.log("createInfoblock1 loaded");
-})();
-
+(function(){    function createInfoBlock () {        const container = $('#container');        fetch('data.json')            .then(function (response) {                if (response.ok) {                    return response.json();                }                throw new Error('Error during fetch');            })            .then(function (data) {                const templateScript = document.getElementById("services").innerHTML;                const template = Handlebars.compile(templateScript);                const html = template(data);                container.append(html);            })    };    createInfoBlock();$(document).ready(function(){    $('.sidenav').sidenav({        menuWidth: 250,        closeOnClick: true    });    $('.carousel').carousel();    $('.collapsible.expandable').collapsible();    $('.tabs').tabs();    console.log('Doc ready');    $('div.tab-content').hide().first().show(); console.log($('div.tab-content'));    // function tabs () {    //     event.preventDefault();    //     $(this).toggleClass('active');    //     $(this).siblings('.tab').removeClass('active');    //     var divID = $(this).children().attr('href');    //     $('div.tab-content').hide();    //     $(divID).show();    // }    // $('.tab').click(tabs);    // $('.tab').focus(tabs);});})();
